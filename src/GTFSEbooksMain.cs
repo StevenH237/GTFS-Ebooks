0205b9a846f19ba95f632db38e6c3d2cb2922221
Steven <github@steven-h.net>
2023-02-20 13:07:32 -0500

    Some progress

--- src/GTFSEbooksMain.cs
+++ src/GTFSEbooksMain.cs
@@ -1,3 +1,9 @@
+using Nixill.GTFS.Entities;
+using Nixill.GTFS.Feeds;
+using Nixill.GTFS.Sources;
+using NodaTime;
+using NodaTime.Text;
+
 namespace Nixill.GTFSBooks;
 
 public class GTFSBooksMain
@@ -5,16 +11,39 @@ public class GTFSBooksMain
   static void Main(string[] args)
   {
     // First get the GTFS file to open.
-    string file;
+    LenientGTFSFeed feed = GetGTFSFeed(args);
 
+    // Now get the current time
+    string timestamp = LocalDateTimePattern.CreateWithInvariantCulture("uuuu-MM-dd-HH-mm-ss").Format(
+      SystemClock.Instance.GetCurrentInstant().WithOffset(Offset.Zero).LocalDateTime
+    );
+
+    // Now create the output
+    string dir = $@"out\{timestamp}\";
+    Directory.CreateDirectory(dir);
+
+    foreach (Route rt in feed.Routes)
+    {
+
+    }
+  }
+
+  static LenientGTFSFeed GetGTFSFeed(string[] args)
+  {
+    string path;
     if (args.Length > 0)
     {
-      file = args[0];
+      path = args[0];
     }
     else
     {
       Console.Write("Please select a GTFS file: ");
-      file = (string)Console.ReadLine();
+      path = Console.ReadLine() ?? "";
     }
+
+    if (File.Exists(path)) return new LenientGTFSFeed(new ZipGTFSDataSource(path));
+    else if (Directory.Exists(path)) return new LenientGTFSFeed(new DirectoryGTFSDataSource(path));
+
+    throw new FileNotFoundException("Does not exist:", path);
   }
 }
